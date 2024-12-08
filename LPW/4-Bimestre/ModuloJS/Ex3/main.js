9cf5c896e4782471452947cb65bae7414ca316db
Everton46HH <everton9343@gmail.com>
2024-12-08 09:11:24 -0300

    Usando Node.js

--- LPW/4-Bimestre/ModuloJS/Ex3/main.js
+++ LPW/4-Bimestre/ModuloJS/Ex3/main.js
@@ -1,6 +1,6 @@
 import { charCounter , wordCounter } from './func.js'
 
-const texto = "Exemplo de uma frase para contar palavras e caracteres."
+const texto = "Everton"
 
 console.log("Numero de carcateres : " , charCounter(texto));
 console.log("Numero de palavras : " , wordCounter(texto));
